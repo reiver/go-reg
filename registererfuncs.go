e4e6c97e1e5457ca2c2d46ca7c66a985e78f1596
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-08-24 18:21:27 -0700

    reg.RegistererFuncs[T]

---
+++ registererfuncs.go
@@ -0,0 +1,26 @@
+package reg
+
+type RegistererFuncs[T any] struct {
+	GetFunc   func(name string) (value T, found bool)
+	LenFunc   func() int
+	SetFunc   func (name string, value T) (previous T, found bool)
+	UnsetFunc func(name string) (previous T, found bool)
+}
+
+var _ Registerer[uint64] = RegistererFuncs[uint64]{}
+
+func (receiver RegistererFuncs[T]) Get(name string) (value T, found bool) {
+	return receiver.GetFunc(name)
+}
+
+func (receiver RegistererFuncs[T]) Len() int {
+	return receiver.LenFunc()
+}
+
+func (receiver RegistererFuncs[T]) Set(name string, value T) (previous T, found bool) {
+	return receiver.Set(name, value)
+}
+
+func (receiver RegistererFuncs[T]) Unset(name string) (previous T, found bool) {
+	return receiver.Unset(name)
+}
